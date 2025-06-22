23eea7f6e3484f40e0a340d1a4803f67b57a7bcf
Danielsodera <daniel_sodera@me.com>
2025-06-22 14:02:13 -0400

    written merge method which satisies rspec

--- lib/main.rb
+++ lib/main.rb
@@ -1,16 +1,21 @@
 # Task manager app 
 # Aim: compare two task lists and merge the two, accounting for duplicates 
 
-tasks_1 = [
-  { title: "Submit report", due_date: "2025-06-22" },
-  { title: "Email client", due_date: "2025-06-23" }
-]
+# tasks_1 = [
+#   { title: "Submit report", due_date: "2025-06-22" },
+#   { title: "Email client", due_date: "2025-06-23" }
+# ]
 
-tasks_2 = [
-  { title: "Submit report", due_date: "2025-06-22" },
-  { title: "Prepare deck", due_date: "2025-06-24" }
-]
+# tasks_2 = [
+#   { title: "Submit report", due_date: "2025-06-22" },
+#   { title: "Prepare deck", due_date: "2025-06-24" }
+# ]
 
 def merge(list_1, list_2)
+  output = list_1
+
+  list_2.each { |task| output << task unless output.include?(task) }
+
+  output
   
 end
